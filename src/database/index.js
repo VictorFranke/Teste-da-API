cbec0a5631482290d4434191ea18adaa3d727c22
VictorFranke <inominavel99@gmail.com>
2022-11-17 21:08:32 -0300

    Correção da contatenção

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 mongoose.Promise = global.Promise
 
 require("dotenv").config()
-module.exports = mongoose.connect('mongodb://' + process.env.DBPORT + process.env.DBNAME,{
+module.exports = mongoose.connect('mongodb://${process.env.DBPORT}/${process.env.DBNAME}/',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
